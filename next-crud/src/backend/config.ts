cbc201456ee8d74f12b3f94a4638cc24d1c051b2
Leonardo Pernomian <leonardopernomian@hotmail.com>
2024-11-04 16:50:23 -0300

    corrigindo erros do firebase

--- next-crud/src/backend/config.ts
+++ next-crud/src/backend/config.ts
@@ -1,13 +1,17 @@
 import firebase from "firebase/compat/app";
+import "firebase/compat/firestore"; 
 
-import "firebase/firestore";
+// Configurações do Firebase
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_apiKey,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_authDomain,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_projectId,
+};
+
 
 if (!firebase.apps.length) {
-  firebase.initializeApp({
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_apiKey,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_authDomain,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_projectId,
-  });
+  firebase.initializeApp(firebaseConfig);
 }
 
-export default firebase;
+
+export default firebaseConfig;
--- next-crud/src/backend/db/CollectionClient.ts
+++ next-crud/src/backend/db/CollectionClient.ts
@@ -1,19 +1,22 @@
-import firebase from "../config";
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, doc, setDoc, addDoc, getDoc, getDocs, deleteDoc, FirestoreDataConverter } from "firebase/firestore";
+import firebaseConfig from "../config";
 import Client from "@/core/Clients";
 import RepoClient from "@/core/RepoClient";
 
+// Inicialize o app Firebase e Firestore
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
 export default class CollectionClient implements RepoClient {
-  #conversor = {
+  #conversor: FirestoreDataConverter<Client> = {
     toFirestore(client: Client) {
       return {
         name: client.getName,
         age: client.getAge,
       };
     },
-    fromFirestore(
-      snapshot: firebase.firestore.QueryDocumentSnapshot,
-      options: firebase.firestore.SnapshotOptions
-    ): Client {
+    fromFirestore(snapshot, options): Client {
       const data = snapshot.data(options);
       return new Client(data.name, data.age, snapshot.id);
     },
@@ -21,12 +24,12 @@ export default class CollectionClient implements RepoClient {
 
   async save(client: Client): Promise<Client> {
     if (client?.getId) {
-      await this.collection().doc(client.getId).set(client);
+      await setDoc(doc(db, "clients", client.getId).withConverter(this.#conversor), client);
       return client;
     } else {
-      const docRef = await this.collection().add(client);
-      const doc = await docRef.get();
-      const data = doc.data();
+      const docRef = await addDoc(collection(db, "clients").withConverter(this.#conversor), client);
+      const docSnap = await getDoc(docRef);
+      const data = docSnap.data();
 
       if (!data) {
         throw new Error("Failed to retrieve client data after saving.");
@@ -35,21 +38,17 @@ export default class CollectionClient implements RepoClient {
       return data;
     }
   }
+
   async delete(client: Client): Promise<void> {
-    return this.collection()
-      .doc(client.getId ?? undefined)
-      .delete();
+    return deleteDoc(doc(db, "clients", client.getId ?? ""));
   }
 
   async all(): Promise<Client[]> {
-    const query = await this.collection().get();
-    return query.docs.map((doc) => doc.data()) ?? [];
+    const querySnapshot = await getDocs(collection(db, "clients").withConverter(this.#conversor));
+    return querySnapshot.docs.map((doc) => doc.data());
   }
 
   private collection() {
-    return firebase
-      .firestore()
-      .collection("clients")
-      .withConverter(this.#conversor);
+    return collection(db, "clients").withConverter(this.#conversor);
   }
 }
